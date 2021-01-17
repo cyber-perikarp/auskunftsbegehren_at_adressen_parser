e658f54366037d6c70b9109070d6c07de32766ae
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2021-01-17 19:10:45 +0100

    refactored check_plz_against_database.py

--- check_plz_against_database.py
+++ check_plz_against_database.py
@@ -7,31 +7,36 @@
 
 import csv
 import os
+import argparse
 
-# In diesem Ordner sind wir
+# Current folder
 workDir = os.path.dirname(os.path.realpath(__file__))
 
-# Hardgecodede Parameter
-foldersToIgnore = [".", "..", "upload", ".git"]
+# Ignore this
+foldersToIgnore = [".", "..", "upload", ".git", "docs"]
 
-# Postleitzahlendatenbank einlesen
+parser = argparse.ArgumentParser("generic_csv_exporter.py")
+parser.add_argument("--source", help="Path to inventory.csv. Default ~/auskunftsbegehren_at_adressen")
+args = vars(parser.parse_args())
+sourceFolder = args["source"] if args["source"] else "~/auskunftsbegehren_at_adressen"
+
+# Read in the plz database
 plzFile = open(workDir + "/plz_verzeichnis.csv", newline="")
 plzDict = csv.DictReader(plzFile)
 plz = {}
 for row in plzDict:
     plz[row["PLZ"]] = (row["Ort"], row["Bundesland"])
 
-# Alle Unterordner laden, außer die die wir ignorieren wollen
-for folder in [x for x in sorted(os.listdir(workDir)) if (os.path.isdir(x) and x not in foldersToIgnore)]:
-    # Hier werden schon die csvs geladen
-    for csvFile in [x for x in os.listdir(workDir + "/" + folder) if os.path.splitext(x)[1] == ".csv"]:
-        # Pfad zur csv
-        csvFile = workDir + "/" + folder + "/" + csvFile
+# Iterate through all folders exept the ones we want to ingore
+for folder in [x for x in os.listdir(sourceFolder) if (os.path.isdir(sourceFolder + "/" + x) and x not in foldersToIgnore)]:
+    # Load the csvs
+    for csvFile in [x for x in os.listdir(sourceFolder + "/" + folder) if os.path.splitext(x)[1] == ".csv"]:
+        # Path to csv
+        csvFile = sourceFolder + "/" + folder + "/" + csvFile
 
-        # csv lesen und parsen
+        # read the csv and parse it
         with open(csvFile, newline='') as csvFileReader:
             readFile = csv.DictReader(csvFileReader)
             for record in readFile:
-                # Unvollständige Datensätze werden nicht eingefügt
                 if record["PLZ"]:
-                    ort = plz[record["PLZ"]][0] # Im Fehlerfall fliegt hier eine Exception was die CI Pipeline abbricht
+                    ort = plz[record["PLZ"]][0] # In case the plz is not in the plz database, this throws an exception and stops the CI
