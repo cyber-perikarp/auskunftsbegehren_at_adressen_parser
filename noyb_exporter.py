8c28f83847b53b0120a3c6b2cce7ee2856bc4165
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2021-01-17 20:47:42 +0100

    noyb_exporter from generic.csv

--- noyb_exporter.py
+++ noyb_exporter.py
@@ -6,77 +6,53 @@
 ###################################
 
 import csv
-import os
-import logging
-import chromalog
-import sys
 import argparse
+import sys
+import os
+import logzero
+from logzero import logger as log
+from locale import strxfrm
 
-# CLI Parameter
-parser = argparse.ArgumentParser("noyb_exporter.py")
-parser.add_argument("--loglevel", help="DEBUG, INFO, ERROR, CRITICAL")
-
+#### Config ####
+# CLI Params
+parser = argparse.ArgumentParser("generic_csv_exporter.py")
+parser.add_argument("--loglevel", help="DEBUG, INFO, ERROR, CRITICAL. Default INFO")
+parser.add_argument("--jsonlog", help="Log output as JSON. Default no")
 args = vars(parser.parse_args())
 
-# Logging stuff
-loglevel = getattr(sys.modules["logging"], args["loglevel"].upper() if args["loglevel"] else "INFO")
+# Logging
+loglevelFromCli = getattr(sys.modules["logging"], args["loglevel"].upper() if args["loglevel"] else "INFO")
+jsonLogFromCli = args["jsonlog"].upper() if args["jsonlog"] else "N"
+logzero.loglevel(loglevelFromCli)
 
-chromalog.basicConfig(format="%(message)s", level=loglevel)
-logger = logging.getLogger()
+# Do we want to log as json?
+if (jsonLogFromCli == "Y" or jsonLogFromCli == "YES"):
+    logzero.json()
 
-# In diesem Ordner sind wir
-workDir = os.path.dirname(os.path.realpath(__file__)) + "/.."
+log.debug("Command Line Parameters: {0}".format(args))
 
-# Hardgecodede Parameter
-outFile = workDir + "/noyb.csv"
+# This is the current folder
+workDir = os.path.dirname(os.path.realpath(__file__))
+
+# Defaults
+sourceFile = workDir + "/upload/generic.csv"
+
+# Hardcoded Parameters
+outFile = workDir + "/upload/noyb.csv"
 csvHeader = ["status", "id", "display_name", "legal_name", "url", "department", "street_address", "city", "neighbourhood", "postal_code", "region", "country", "requires_identification", "operating_countries", "custom_identifier", "identifiers", "generic_url", "generic_email", "generic_note", "access_url", "access_email", "access_note", "deletion_url", "deletion_email", "deletion_note", "portability_url", "portability_email", "portability_note", "correction_url", "correction_email", "correction_note"]
-foldersToIgnore = [".", "..", ".exporter", "docs", "upload", ".git", ".github"]
-
-# Postleitzahlendatenbank einlesen
-plzFile = open(workDir + "/.exporter/plz_verzeichnis.csv", newline="")
-plzDict = csv.DictReader(plzFile)
-plz = {}
-for row in plzDict:
-    plz[row["PLZ"]] = (row["Ort"], row["Bundesland"])
-
-def sanitizePhoneNumber(number):
-    number = number.replace(" ", "")
-    number = number.replace("-", "")
-    number = number.replace("/", "")
-    number = number.replace("(", "")
-    number = number.replace(")", "")
-    number = number.replace("'", "") # Wegen LibreOffice
-    logger.debug("Sanitized Phone Number: " + number)
-    return number
 
 def checkIfFullRecord(record):
-    if (not record["Id"]
-        or not record["Name"]
+    if (not record["Name"]
         or not record["Name_Lang"]
         or not record["Adresse"]
         or not record["PLZ"]
         or not record["Pruefung"]
         or not record["E-Mail"]):
-            logger.error("Not exporting: " + record["Name"])
+            log.error("Not exporting: {0}".format(record["Name"]))
             return False
     return True
 
 def populateGeneratedFields(record):
-    # Alle leerzeichen, bindestriche, klammern etc aus telefon und faxnummer entfernen
-    tel = sanitizePhoneNumber(record["Tel"])
-    fax = sanitizePhoneNumber(record["Fax"])
-
-    # Postleitzahl und region aus Postleitzahlendatenbank
-    city = plz[record["PLZ"]][0]
-    region = plz[record["PLZ"]][1]
-
-    logger.debug("Found city and region: " + city + ", " + region)
-
-    # ID
-    sourceFile = os.path.splitext(csvFile)[0].split("/")[-1]
-    lastChecked = record["Pruefung"].replace(".", "-")
-    id = sourceFile + "_" + record["Folder"] + "_" + record["Id"] + "_" + lastChecked
-
     recordToReturn = {}
 
     # Felder für noyb
@@ -84,21 +60,21 @@ def populateGeneratedFields(record):
     recordToReturn["id"] = ""
     recordToReturn["display_name"] = record["Name"]
     recordToReturn["legal_name"] = record["Name_Lang"]
-    recordToReturn["url"] = ""
+    recordToReturn["url"] = record["Homepage"]
     recordToReturn["department"] = ""
     recordToReturn["street_address"] = record["Adresse"]
-    recordToReturn["city"] = city
+    recordToReturn["city"] = record["Ort"]
     recordToReturn["neighbourhood"] = ""
     recordToReturn["postal_code"] = record["PLZ"]
-    recordToReturn["region"] = region
+    recordToReturn["region"] = record["Bundesland"]
     recordToReturn["country"] = "AT"
     recordToReturn["requires_identification"] = ""
     recordToReturn["operating_countries"] = ""
-    recordToReturn["custom_identifier"] = id
+    recordToReturn["custom_identifier"] = "{0}_{1}_{2}_{3}".format(record["Branche"], record["Ebene"], record["Ort"].replace(" ", "-"), record["Pruefung"].replace(".", "-"))
     recordToReturn["identifiers"] = ""
     recordToReturn["generic_url"] = ""
     recordToReturn["generic_email"] = record["E-Mail"]
-    recordToReturn["generic_note"] = "Phone: " + tel + ", Fax: " + fax
+    recordToReturn["generic_note"] = "Phone: {0}, Fax: {1}".format(record["Tel"], record["Fax"])
     recordToReturn["access_url"] = ""
     recordToReturn["access_email"] = ""
     recordToReturn["access_note"] = ""
@@ -114,41 +90,32 @@ def populateGeneratedFields(record):
 
     return recordToReturn
 
-# Header schreiben
+# Write Header
 try:
     with open(outFile, "w") as outFileHandler:
-        logger.debug("Headers: " + str(csvHeader))
-        writer = csv.DictWriter(outFileHandler, fieldnames=csvHeader)
+        log.debug("Headers: {0}".format(str(csvHeader)))
+        writer = csv.DictWriter(outFileHandler, fieldnames=csvHeader, quoting=csv.QUOTE_ALL)
         writer.writeheader()
 except IOError:
-    logger.critical("Cant write header to file!")
+    log.critical("Cant write to file!")
+    exit(1)
 
-logger.debug(sorted(os.listdir(workDir)))
+# read the file
+with open(sourceFile, newline='') as csvFileReader:
+    readFile = csv.DictReader(csvFileReader)
+    for record in readFile:
+        # only insert records that have the required fields
+        if (checkIfFullRecord(record)):
+            log.debug("Processing entry: {0}".format(record["Name"]))
+            record = populateGeneratedFields(record)
+            log.debug(record)
+            try:
+                with open(outFile, "a+") as outFileHandler:
+                    log.debug("Writing entry: {0}".format(record["display_name"]))
 
-# Alle Unterordner laden, außer die die wir ignorieren wollen
-for folder in [x for x in sorted(os.listdir(workDir)) if (os.path.isdir(x) and x not in foldersToIgnore)]:
-    # Hier werden schon die csvs geladen
-    for csvFile in [x for x in os.listdir(workDir + "/" + folder) if os.path.splitext(x)[1] == ".csv"]:
-        # Pfad zur csv
-        csvFile = workDir + "/" + folder + "/" + csvFile
-        logger.info("Using File: " + csvFile)
+                    writer = csv.DictWriter(outFileHandler, fieldnames=csvHeader, quoting=csv.QUOTE_ALL)
+                    writer.writerow(record)
 
-        # csv lesen und parsen
-        with open(csvFile, newline='') as csvFileReader:
-            readFile = csv.DictReader(csvFileReader)
-            for record in readFile:
-                record["Folder"] = folder # Wir brauchen das zum generieren der ID
-                # Unvollständige Datensätze werden nicht eingefügt
-                if (checkIfFullRecord(record)):
-                    logger.debug("Processing entry: " + record["Name"])
-                    record = populateGeneratedFields(record)
-                    logger.debug(record)
-
-                    # CSV schreiben!
-                    try:
-                        with open(outFile, "a+") as outFileHandler:
-                            writer = csv.DictWriter(outFileHandler, fieldnames=csvHeader)
-                            writer.writerow(record)
-
-                    except IOError:
-                        logger.critical("Cant write entry to file!")
+            except IOError:
+                log.critical("Cant write to file!")
+                exit(1)
